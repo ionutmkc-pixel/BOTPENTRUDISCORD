c87faf811fb1a32a721eba747424ae0f93e520dd
ionutmkc-pixel <ionutmkc@gmail.com>
2026-01-30 18:18:01 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,19 +4,16 @@ import requests
 import xml.etree.ElementTree as ET
 import os
 
-# INTENTS – OBLIGATORIU
+# INTENTS
 intents = discord.Intents.default()
-intents.message_content = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# CANAL DISCORD
-CANAL_ID = 1466767151267446953
+# ID CANAL VOICE (al tău)
+VOICE_CHANNEL_ID = 1466767151267446953
 
-# XML FS25
+# Link XML FS25
 XML_URL = "http://85.190.163.102:10710/feed/dedicated-server-stats.xml?code=0c77cbd246bbdae1ad09d6ef78780e78"
 
-# LUNI
 LUNI = {
     1: "IAN", 2: "FEB", 3: "MAR", 4: "APR",
     5: "MAI", 6: "IUN", 7: "IUL", 8: "AUG",
@@ -27,10 +24,10 @@ def timp_fs25():
     r = requests.get(XML_URL, timeout=5)
     root = ET.fromstring(r.content)
 
-    data = root.find(".//gameDate").text      # YYYY-MM-DD
-    timp = root.find(".//gameTime").text      # HH:MM:SS
+    data = root.find(".//gameDate").text
+    timp = root.find(".//gameTime").text
 
-    an, luna, zi = map(int, data.split("-"))
+    an, luna, _ = map(int, data.split("-"))
     ora, minut, _ = map(int, timp.split(":"))
 
     total_min = (ora * 60 + minut) * 3
@@ -39,22 +36,17 @@ def timp_fs25():
     ora_joc = total_min // 60
     min_joc = total_min % 60
 
-    return f"{an} | {LUNI[luna]} | {ora_joc:02d}:{min_joc:02d} | x3"
+    return f"🕒 FS25 | {an} | {LUNI[luna]} | {ora_joc:02d}:{min_joc:02d} x3"
 
 @bot.event
 async def on_ready():
     print("BOT PORNIT")
-    postare.start()
+    update_voice_name.start()
 
 @tasks.loop(minutes=5)
-async def postare():
-    canal = bot.get_channel(CANAL_ID)
+async def update_voice_name():
+    canal = bot.get_channel(VOICE_CHANNEL_ID)
     if canal:
-        await canal.send(timp_fs25())
+        await canal.edit(name=timp_fs25())
 
-@bot.command()
-async def timp(ctx):
-    await ctx.send(timp_fs25())
-
-# TOKEN DIN RAILWAY ENV
 bot.run(os.environ["DISCORD_TOKEN"])
